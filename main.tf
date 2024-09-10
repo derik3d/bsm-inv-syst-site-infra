498f6b08defe6cea96742951775d4318b329d72a
Derik Daniel Daza <derik3d@gmail.com>
2024-09-10 12:34:31 -0500

    updated handler for lambda

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_apigatewayv2_api" "api_bigstoremanager" {
 
 resource "aws_lambda_function" "lambda_bigstoremanager_api" {
   function_name = "lambda-bigstoremanager-api"
-  handler       = "app.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_exec_role.arn
 
